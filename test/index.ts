fef7848e563a2b7b9630eb4b9da0ff930dec4260
Mark Lee <electronjs@lazymalevolence.com>
2020-03-23 00:22:57 -0700

    feat: throw a separate error when a process terminates via signal

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,5 @@
-import { CrossSpawnError, spawn } from "../src";
+import { CrossSpawnError, ExitSignalError, spawn } from "../src";
+import path from "path";
 import test from "ava";
 
 test("returns stdout", async (t) => {
@@ -23,6 +24,20 @@ test("throws an error when it cannot find an executable", async (t) => {
 
   t.is(originalError.code, "ENOENT");
   t.is(originalError.syscall, "spawn does-not-exist");
+});
+
+test("throws an error when the spawned process is terminated via a signal", async (t) => {
+  const error = (await t.throwsAsync(
+    spawn(path.resolve(__dirname, "..", "node_modules", ".bin", "ts-node"), [
+      path.resolve(__dirname, "fixtures", "killed-by-signal.ts"),
+    ]),
+    {
+      instanceOf: ExitSignalError,
+      message: /^Command terminated via a signal/,
+    }
+  )) as ExitSignalError;
+
+  t.is(error.signal, "SIGKILL");
 });
 
 test("updateErrorCallback modifies the exception", async (t) => {
